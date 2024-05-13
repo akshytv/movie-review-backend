e5895daef481a46c34d6a0314f7c7baf1c17854a
akshy <akshaytv88@gmail.com>
2024-05-13 14:44:52 +0530

    first commit full project

---
+++ src/test/java/com/akshay/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
